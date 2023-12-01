010b967107e1b94e19219979960084e75ae54be2
Alex Dykstra <alex@alexdykstra.com>
2023-12-01 10:11:39 -0500

    2023 day 1

--- twentytwentythree/src/main.rs
+++ twentytwentythree/src/main.rs
@@ -1,31 +1,68 @@
-use std::env;
-use std::fs;
+use std::env::{self};
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
     let file_path = &args[1];
-    println!("In file {}", file_path);
-
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-
-    let mut current_floor = 0;
-    let mut characters_processed = 0;
-    let mut entered_basement = false;
-    let contents_itr = contents.char_indices();
-    for character in contents_itr {
-        characters_processed += 1;
-        let directive = character.1;
-        if directive == ')' {
-            current_floor -= 1;
+    if let Ok(lines) = read_lines(file_path) {
+        let mut total: i32 = 0;
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(word) = line {
+                println!("{}", word);
+                let word_number = get_word_number(word);
+                println!("{}", word_number);
+                total += word_number;
+            }
         }
-        if directive == '(' {
-            current_floor += 1;
-        }
-        if current_floor == (-1) && !entered_basement {
-            println!("in the basement first: {}", characters_processed);
-            entered_basement = true;
+        println!("Total: {}", total)
+    }
+}
+
+fn make_word_numbers_numbers(word: String) -> String {
+    return word
+        .replace("twone", "twoone")
+        .replace("nineight", "nineeight")
+        .replace("eightwo", "eighttwo")
+        .replace("eighthree", "eightthree")
+        .replace("oneight", "oneeight")
+        .replace("one", "1")
+        .replace("two", "2")
+        .replace("three", "3")
+        .replace("four", "4")
+        .replace("five", "5")
+        .replace("six", "6")
+        .replace("seven", "7")
+        .replace("eight", "8")
+        .replace("nine", "9");
+}
+
+fn get_word_number(unclean_word: String) -> i32 {
+    // not 56002 (too high)
+    // not 55680 (too low)
+    let word = make_word_numbers_numbers(unclean_word);
+    let mut first_num: char;
+    for c in word.chars() {
+        if c.is_digit(10) {
+            first_num = c;
+            for c_2 in word.chars().rev() {
+                if c_2.is_digit(10) {
+                    return format!("{}{}", first_num, c_2).parse().unwrap();
+                }
+            }
         }
     }
-    println!("Floor: {}", current_floor)
+    return 0;
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
